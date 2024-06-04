0c0f873c2f0678b31e3ff8ed2462fff2455992ec
Martin <marttinguerreroo@gmail.com>
2024-06-04 01:27:48 -0300

    Subo al nuevo repo el bot seteado

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/marttinguerrero/bot_futbol
 
 go 1.22.3
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
--- main.go
+++ main.go
@@ -1,7 +1,66 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+	"strings"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func run() {
+	apiToken := os.Getenv("TELEGRAM_APITOKEN")
+	if apiToken == "" {
+		log.Fatal("TELEGRAM_APITOKEN environment variable is required")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(apiToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	var lista []string
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message updates
+			continue
+		}
+
+		if !update.Message.IsCommand() { // ignore any non-command Messages
+			continue
+		}
+
+		// Create a new MessageConfig. We don't have text yet,
+		// so we leave it empty.
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+		// Extract the command from the Message.
+		switch update.Message.Command() {
+		case "help":
+			msg.Text = "I understand /sayhi and /status."
+		case "sumo":
+			lista = append(lista, update.Message.From.FirstName)
+			msg.Text = "Jugadores que suman al partido: " + strings.Join(lista, ", ")
+		case "status":
+			msg.Text = "I'm ok."
+		default:
+			msg.Text = "I don't know that command"
+		}
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Panic(err)
+		}
+	}
+}
 
 func main() {
-	log.Println("Hola")
+	run()
 }
